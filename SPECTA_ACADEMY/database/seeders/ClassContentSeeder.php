ec36374da24f668d82e1b2cc4ea8eb0989e1a8de
Stevens2606 <stevensimanjuntak2606@gmail.com>
2026-03-04 10:59:29 +0700

    save local seeder

---
+++ SPECTA_ACADEMY/database/seeders/ClassContentSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClassContentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
